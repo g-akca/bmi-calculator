cc2018192b729147b97136eaf42b8e31fcdc5d68
g-akca <gok.akca66@gmail.com>
2026-03-20 02:40:44 +0300

    Finish TipsItem content

--- src/components/TipsItem.jsx
+++ src/components/TipsItem.jsx
@@ -1,7 +1,12 @@
-function TipsItem() {
+function TipsItem({ tip }) {
   return (
-    <section>
+    <section className="flex flex-col gap-8">
+      <img src={tip.icon} alt={tip.name} className="w-16 aspect-square" />
 
+      <div className="flex flex-col gap-6">
+        <h3 className="text-[24px] leading-7.25 font-semibold tracking-[-4%] text-blue-900">{tip.name}</h3>
+        <p>{tip.description}</p>
+      </div>
     </section>
   )
 }
