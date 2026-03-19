670dc312e211bce2a40307a4c17859ab033b5c0e
g-akca <gok.akca66@gmail.com>
2026-03-20 02:42:35 +0300

    Finish tips list

--- src/components/TipsSection.jsx
+++ src/components/TipsSection.jsx
@@ -7,16 +7,29 @@ import TipsItem from "./TipsItem";
 function TipsSection() {
   const tips = [
     {
+      id: 1,
       icon: eatingIcon,
       name: "Healthy eating",
       description: "Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood."
-    }
+    },
+    {
+      id: 2,
+      icon: exerciseIcon,
+      name: "Regular exercise",
+      description: "Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity."
+    },
+    {
+      id: 3,
+      icon: sleepIcon,
+      name: "Adequate sleep",
+      description: "Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation."
+    },
   ]
 
   return (
     <section className="px-6 py-12 flex flex-col gap-10">
       {tips.map(item => (
-        <TipsItem tip={item} />
+        <TipsItem key={item.id} tip={item} />
       ))}
     </section>
   )
