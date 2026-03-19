cea52a8d441e9405ea2f77ea19f2555269f9e76d
g-akca <gok.akca66@gmail.com>
2026-03-20 02:11:55 +0300

    Add HeroCalculator styles

--- src/components/HeroCalculator.jsx
+++ src/components/HeroCalculator.jsx
@@ -1,41 +1,55 @@
 function HeroCalculator() {
   return (
     <form className="bg-white w-full p-6 rounded-2xl flex flex-col gap-6">
-      <h2>Enter your details below</h2>
+      <h2 className="text-blue-900 font-semibold text-[24px] leading-7.25 tracking-[-4%]">Enter your details below</h2>
 
       <fieldset className="grid grid-cols-2 gap-6">
-        <label>
-          <input type="radio" name="unit" id="metric" value="metric" required />
-          <span>Metric</span> 
+        <label className="flex gap-4 items-center">
+          <input 
+            type="radio" name="unit" value="metric" required checked 
+            className="
+              appearance-none w-7.75 h-7.75 rounded-full border border-grey-500 flex items-center justify-center
+              checked:bg-blue-100 checked:border-0 checked:before:content[''] checked:before:w-3.75 checked:before:h-3.75 
+              checked:before:bg-blue-500 checked:before:rounded-full
+            " 
+          />
+          <span className="font-semibold text-blue-900">Metric</span> 
         </label>
 
-        <label>
-          <input type="radio" name="unit" id="imperial" value="imperial" required />
-          <span>Imperial</span> 
+        <label className="flex gap-4 items-center">
+          <input 
+            type="radio" name="unit" value="imperial" required checked 
+            className="
+              appearance-none w-7.75 h-7.75 rounded-full border border-grey-500 flex items-center justify-center
+              checked:bg-blue-100 checked:border-0 checked:before:content[''] checked:before:w-3.75 checked:before:h-3.75 
+              checked:before:bg-blue-500 checked:before:rounded-full
+            " 
+          />
+          <span className="font-semibold text-blue-900">Imperial</span> 
         </label>
       </fieldset>
 
       <div className="flex flex-col gap-4">
-        <div>
-          <label htmlFor="height">Height</label>
-          <div>
-            <input type="number" id="height" placeholder="0" />
-            <span>cm</span>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="height" className="text-[14px] leading-base">Height</label>
+          <div className="flex gap-6 p-6 border border-grey-500 rounded-xl text-[24px] leading-7.25 font-semibold tracking-[-4%]">
+            <input type="number" id="height" placeholder="0" min="0" className="grow min-w-0 text-blue-900 placeholder:text-grey-500" />
+            <span className="text-blue-500">cm</span>
           </div>
         </div>
 
-        <div>
-          <label htmlFor="weight">Weight</label>
-          <div>
-            <input type="number" id="weight" placeholder="0" />
-            <span>kg</span>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="weight" className="text-[14px] leading-base">Weight</label>
+          <div className="flex gap-6 p-6 border border-grey-500 rounded-xl text-[24px] leading-7.25 font-semibold tracking-[-4%]">
+            <input type="number" id="weight" placeholder="0" min="0" className="grow min-w-0 text-blue-900 placeholder:text-grey-500" />
+            <span className="text-blue-500">kg</span>
           </div>
         </div>
       </div>
 
       <section className="p-8 bg-blue-500 rounded-2xl text-white flex flex-col gap-6">
-        <h3>Welcome!</h3>
-        <p>Enter your height and weight and you’ll see your BMI result here</p>
+        <h3 className="text-[24px] leading-7.25 font-semibold tracking-[-4%]">Welcome!</h3>
+        <p className="text-[14px] leading-base">Enter your height and weight and you’ll see your BMI result here</p>
       </section>
     </form>
   )
--- src/components/HeroHeading.jsx
+++ src/components/HeroHeading.jsx
@@ -6,7 +6,7 @@ function HeroHeading() {
       <img src={logoImg} alt="Logo image" className="w-10 aspect-square" />
 
       <div className="text-center">
-        <h1 className="mb-6 font-semibold text-[48px] leading-[110%] tracking-[-5%] text-blue-900">Body Mass Index Calculator</h1>
+        <h1 className="mb-6 font-semibold text-[48px] leading-[110%] tracking-[-4%] text-blue-900">Body Mass Index Calculator</h1>
         <p>
           Better understand your weight in relation to your height using our body mass index (BM) calculator. 
           While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to 
