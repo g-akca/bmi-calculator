6dd4076dd7807ea4789e78e63650ed424b842bef
g-akca <gok.akca66@gmail.com>
2026-03-20 01:17:19 +0300

    Add HeroCalculator structure

--- src/components/HeroCalculator.jsx
+++ src/components/HeroCalculator.jsx
@@ -3,13 +3,40 @@ function HeroCalculator() {
     <form className="bg-white w-full p-6 rounded-2xl flex flex-col gap-6">
       <h2>Enter your details below</h2>
 
-      <fieldset>
-        <input type="radio" />
-        <label></label>
+      <fieldset className="grid grid-cols-2 gap-6">
+        <label>
+          <input type="radio" name="unit" id="metric" value="metric" required />
+          <span>Metric</span> 
+        </label>
 
-        <input type="radio" />
-        <label></label>
+        <label>
+          <input type="radio" name="unit" id="imperial" value="imperial" required />
+          <span>Imperial</span> 
+        </label>
       </fieldset>
+
+      <div className="flex flex-col gap-4">
+        <div>
+          <label htmlFor="height">Height</label>
+          <div>
+            <input type="number" id="height" placeholder="0" />
+            <span>cm</span>
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="weight">Weight</label>
+          <div>
+            <input type="number" id="weight" placeholder="0" />
+            <span>kg</span>
+          </div>
+        </div>
+      </div>
+
+      <section className="p-8 bg-blue-500 rounded-2xl text-white flex flex-col gap-6">
+        <h3>Welcome!</h3>
+        <p>Enter your height and weight and you’ll see your BMI result here</p>
+      </section>
     </form>
   )
 }
