1429d9874acec7fb356de8d0b94e30b046f2d149
g-akca <gok.akca66@gmail.com>
2026-03-20 04:00:26 +0300

    Remove focus outline from inputs

--- src/components/HeroCalculator.jsx
+++ src/components/HeroCalculator.jsx
@@ -9,8 +9,8 @@ function HeroCalculator() {
             type="radio" name="unit" value="metric" required defaultChecked 
             className="
               appearance-none w-7.75 h-7.75 rounded-full border border-grey-500 flex items-center justify-center
-              checked:bg-blue-100 checked:border-0 checked:before:content[''] checked:before:w-3.75 checked:before:h-3.75 
-              checked:before:bg-blue-500 checked:before:rounded-full
+              cursor-pointer hover:border-blue-500 checked:bg-blue-100 checked:border-0 checked:before:content[''] 
+              checked:before:w-3.75 checked:before:h-3.75 checked:before:bg-blue-500 checked:before:rounded-full
             " 
           />
           <span className="font-semibold text-blue-900">Metric</span> 
@@ -21,8 +21,8 @@ function HeroCalculator() {
             type="radio" name="unit" value="imperial" required
             className="
               appearance-none w-7.75 h-7.75 rounded-full border border-grey-500 flex items-center justify-center
-              checked:bg-blue-100 checked:border-0 checked:before:content[''] checked:before:w-3.75 checked:before:h-3.75 
-              checked:before:bg-blue-500 checked:before:rounded-full
+              cursor-pointer hover:border-blue-500 checked:bg-blue-100 checked:border-0 checked:before:content[''] 
+              checked:before:w-3.75 checked:before:h-3.75 checked:before:bg-blue-500 checked:before:rounded-full
             " 
           />
           <span className="font-semibold text-blue-900">Imperial</span> 
@@ -32,16 +32,16 @@ function HeroCalculator() {
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-2">
           <label htmlFor="height" className="text-[14px] leading-base">Height</label>
-          <div className="flex gap-6 p-6 border border-grey-500 rounded-xl text-[24px] leading-7.25 font-semibold tracking-[-4%]">
-            <input type="number" id="height" placeholder="0" min="0" className="grow min-w-0 text-blue-900 placeholder:text-grey-500" />
+          <div className="flex gap-6 p-6 border border-grey-500 hover:border-blue-500 rounded-xl text-[24px] leading-7.25 font-semibold tracking-[-4%]">
+            <input type="number" id="height" placeholder="0" min="0" className="grow min-w-0 text-blue-900 placeholder:text-grey-500 focus:outline-none" />
             <span className="text-blue-500">cm</span>
           </div>
         </div>
 
         <div className="flex flex-col gap-2">
           <label htmlFor="weight" className="text-[14px] leading-base">Weight</label>
-          <div className="flex gap-6 p-6 border border-grey-500 rounded-xl text-[24px] leading-7.25 font-semibold tracking-[-4%]">
-            <input type="number" id="weight" placeholder="0" min="0" className="grow min-w-0 text-blue-900 placeholder:text-grey-500" />
+          <div className="flex gap-6 p-6 border border-grey-500 hover:border-blue-500 rounded-xl text-[24px] leading-7.25 font-semibold tracking-[-4%]">
+            <input type="number" id="weight" placeholder="0" min="0" className="grow min-w-0 text-blue-900 placeholder:text-grey-500 focus:outline-none" />
             <span className="text-blue-500">kg</span>
           </div>
         </div>
